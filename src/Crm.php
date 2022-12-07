2e9afe90be05240dd6f1f0e288405bf17458c46c
sahib-singh-mdh <101691940+sahib-singh-mdh@users.noreply.github.com>
2022-12-07 14:04:09 +0530

    By Default Special Case: Null

--- src/Crm.php
+++ src/Crm.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class Crm {
 
-    public function init($apiUrl, $body = null, $method, $auth, $specialCase = false)
+    public function init($apiUrl, $body = null, $method, $auth, $specialCase = null)
     {
         $client = new Client();
         $apiKey = $auth['apiKey'];
